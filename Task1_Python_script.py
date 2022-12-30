6b63d4f0b40b9328fdd9e2159cd791167b7429be
rashmi-pujara <71028855+rashmi-pujara@users.noreply.github.com>
2022-12-30 12:06:55 -0500

    BeautifulSoup Library

--- Task1_Python_script.py
+++ Task1_Python_script.py
@@ -1,3 +1,4 @@
+#beautifulsoup
 # Python script to scrape an article given the url of the article and store the extracted text in a file
 # Url: https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7
 
